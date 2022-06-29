15965b4f28ea2ec8f016b2143784c615720fa838
Shyam <Shm24677@gmail.com>
2022-06-29 17:07:49 +0530

    Banner component Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from 'react'
 import './App.css';
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar';
+import Rowpost from './Components/Rowpost/Rowpost';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Banner />
+      <Rowpost />
     </div>
   );
 }
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {API_KEY,imageUrl} from '../../constants'
 import './Banner.css'
-
+import axios from '../../axios'
 function Banner() {
+  const[movie,setMovie] = useState()
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      setMovie(response.data.results[Math.floor((Math.random() * 20) + 1)])
+    })
+  },[])
+ console.log(movie);
   return (
-    <div className='banner'>
+    <div style={{backgroundImage :`url(${movie ? imageUrl+movie.backdrop_path : ""})`}} className='banner'>
         <div className="content">
-            <h1 className="title">Movie Name</h1>
+            <h1 className="title">{movie ? movie.title : ''}</h1>
             <div className="bannerButtons">
                 <button className="button">Play</button>
                 <button className="button">My List</button>
             </div>
-            <h5 className="description">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the</h5>
+            <h5 className="description">{movie ? movie.overview : ''}</h5>
         </div>
         <div className="fade_bottom"></div>
     </div>
